6c7d8c3a473bb4d99d0d9446f66f3b17734a73b6
Diego A. Bueno Vitor <abuvitordiego.contato@gmail.com>
2026-02-25 22:52:24 -0300

    initial commit projeto de Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyComponent } from './my-component/my-component';
+import './my-component/my-component.css'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
